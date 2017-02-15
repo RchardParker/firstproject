dbcc5ffd54b4c6dac7060bc5f619ff15009a8be7
drenner <drenner@PC04111873>
2017-02-15 11:51:00 +0100

    WennDannSonst erweiterung

--- mixed_app/mixed_app/mixed_app.cpp
+++ mixed_app/mixed_app/mixed_app.cpp
@@ -10,9 +10,10 @@ int main()
 {
 	int a = 0;
 
+	/*
 	system("COLOR");	//gibt an, dass es um Farben geht
 
-						/*
+						
 						printf("\033[46m++++++++Auswahl++++++++\033[m\n");	// \033 bedeutet eine Farbwahl, [46m gibt den Farbton an, und beides wird wieder mit \033[m beendet
 						printf("\033[1m Bitte Auswahl treffen  \033[m\n");
 						printf("\033[1m K = Kindergeburtstag   \033[m\n");
@@ -25,8 +26,16 @@ int main()
 	printf("S = Scan berechnen\n");
 	scanf("%c", &a);
 
-	a == 115 || a == 83 ? fScan() : fKinder();
-
+	// a == 115 || a == 83 ? fScan() : a == 75 || a == 107 ? fKinder();	//Erste Variante
+	
+	if (a == 115 || a == 83) {
+		fScan();
+	}
+	else if (a == 75 || a == 107){
+		fKinder();
+	}
+	
+	printf("Falsche eingabe!\nStarten Sie das Programm neu.\n");
 
 	return 0;
 }
